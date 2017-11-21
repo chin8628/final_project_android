ea10f445ac94de69175509039d194a402a3ec908
chin8628 <boon.8628@gmail.com>
2017-11-21 08:49:56 +0700

    First commit

---
+++ app/src/main/java/kmitl/fina/boonyarith58070077/bnk48feed/MainActivity.java
@@ -0,0 +1,15 @@
+package kmitl.fina.boonyarith58070077.bnk48feed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
