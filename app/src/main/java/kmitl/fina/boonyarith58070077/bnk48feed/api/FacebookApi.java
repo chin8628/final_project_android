0659eccedfdba5eee5d07ebc882cac27c38b56bb
chin8628 <boon.8628@gmail.com>
2017-11-22 09:16:29 +0700

    Added api

---
+++ app/src/main/java/kmitl/fina/boonyarith58070077/bnk48feed/api/FacebookApi.java
@@ -0,0 +1,15 @@
+package kmitl.fina.boonyarith58070077.bnk48feed.api;
+
+import kmitl.fina.boonyarith58070077.bnk48feed.model.facebook.feed.FacebookModel;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface FacebookApi {
+
+    String BASE = "http://www.cloudian.in.th";
+
+    @GET("/bnk48/{user}")
+    Call<FacebookModel> getFeed(@Path("user") String user);
+
+}
